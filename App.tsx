8ad5d43bc0223e7b7b0fc23354d3826e3482675f
ALUIZIO NETO <aluizio.sln@gmail.com>
2023-06-05 19:31:39 -0400

    feat: add home screen style

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, Text, View } from "react-native";
+import blurBg from "./assets/bg-blur.png";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import Stripes from "./assets/stripes.svg";
+
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+import { styled } from "nativewind";
 
 export default function App() {
+  const StyledStripes = styled(Stripes);
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={blurBg}
+      className=" bg-gray-950 flex-1 items-center relative"
+      imageStyle={{ position: "absolute", left: "-100%" }}
+    >
+      <StyledStripes className="absolute left-2" />
+      <Text className=" text-zinc-50 text-5xl font-alt"></Text>
+      <StatusBar style="light" />
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
